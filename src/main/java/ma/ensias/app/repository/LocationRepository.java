d1737186d0b4dfbb71b06830df181a96b327ebc9
Ayoub9LE <ayoublemniai9@gmail.com>
2023-06-24 12:16:32 +0000

    location / controller

--- src/main/java/ma/ensias/app/repository/LocationRepository.java
+++ src/main/java/ma/ensias/app/repository/LocationRepository.java
@@ -1,42 +1,17 @@
 package ma.ensias.app.repository;
 
-
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-
-import jakarta.transaction.Transactional;
 import ma.ensias.app.entity.Location;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+
+import java.time.LocalDate;
 import java.util.List;
 
-public class LocationRepository {
-    @PersistenceContext
-    private EntityManager em;
+@Repository
+public interface LocationRepository extends JpaRepository<Location, Long> {
 
-    public Location find(Long id) {
-        return em.find(Location.class, id);
-    }
-
-    @Transactional
-    public Location save(Location location) {
-        if (location.getId() == null) {
-            em.persist(location);
-            return location;
-        } else {
-            return em.merge(location);
-        }
-    }
-
-    @Transactional
-    public void delete(Long id) {
-        Location location = em.find(Location.class, id);
-        if (location != null) {
-            em.remove(location);
-        }
-    }
-
-    public List<Location> findAll() {
-        return em.createQuery("SELECT l FROM Location l", Location.class).getResultList();
-    }
+    @Query("SELECT l FROM Location l WHERE l.startDate >= ?1 and l.endDate <= ?2")
+    List<Location> findLocationsByDateRange(LocalDate startDate, LocalDate endDate);
 }
-
--- src/main/java/ma/ensias/app/service/LocationService.java
+++ src/main/java/ma/ensias/app/service/LocationService.java
@@ -2,27 +2,61 @@ package ma.ensias.app.service;
 
 
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
+
+import ma.ensias.app.entity.Location;
+import ma.ensias.app.repository.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.Objects;
+
+@Service
 public class LocationService {
+    private final LocationRepository locationRepository;
 
-    @Inject
-    private LocationRepository repository;
-
-    public Location find(Long id) {
-        return repository.find(id);
+    @Autowired
+    public LocationService(LocationRepository locationRepository) {
+        this.locationRepository = locationRepository;
     }
 
-    public Location save(Location location) {
-        return repository.save(location);
+    public List<Location> getLocations() {
+        return locationRepository.findAll();
     }
 
-    public void delete(Long id) {
-        repository.delete(id);
+    public void addLocation(Location location) {
+        locationRepository.save(location);
     }
 
-    public List<Location> findAll() {
-        return repository.findAll();
+    public void deleteLocation(Long locationId) {
+        if(!locationRepository.existsById(locationId)) {
+            throw new IllegalStateException("Location with id "+locationId+" does not exist");
+        }
+        locationRepository.deleteById(locationId);
     }
-}
 
+    @Transactional
+    public void updateLocation(Long locationId,
+                               LocalDate startDate,
+                               LocalDate endDate,
+                               BigDecimal price,
+                               Boolean isPaid) {
+        Location location = locationRepository.findById(locationId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "Location with id "+locationId+" does not exist"));
+
+        if (startDate != null && !Objects.equals(location.getStartDate(), startDate)) {
+            location.setStartDate(startDate);
+        }
+
+        if (endDate != null && !Objects.equals(location.getEndDate(), endDate)) {
+            location.setEndDate(endDate);
+        }
+
+
+}}
