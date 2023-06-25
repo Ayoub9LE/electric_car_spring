788b25ef423562b4864bb5314e621a810da90a35
Ayoub9LE <ayoublemniai9@gmail.com>
2023-06-25 13:01:04 +0000

    agency controller

---
+++ src/main/java/ma/ensias/app/controller/AgencyController.java
@@ -0,0 +1,39 @@
+package ma.ensias.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/agencies")
+public class AgencyController {
+
+    @Autowired
+    private AgencyService agencyService;
+
+    @GetMapping
+    public List<Agency> getAllAgencies() {
+        return agencyService.getAllAgencies();
+    }
+
+    @GetMapping("/{id}")
+    public Agency getAgencyById(@PathVariable Long id) {
+        return agencyService.getAgencyById(id).orElseThrow(() -> new RuntimeException("Agency not found"));
+    }
+
+    @PostMapping
+    public Agency createAgency(@RequestBody Agency agency) {
+        return agencyService.createAgency(agency);
+    }
+
+    @PutMapping("/{id}")
+    public Agency updateAgency(@PathVariable Long id, @RequestBody Agency agency) {
+        // You might want to validate or set the id from the path into the agency object here
+        return agencyService.updateAgency(agency);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAgency(@PathVariable Long id) {
+        agencyService.deleteAgency(id);
+    }
+}
